a5bd604b0952cfae12c5be4621ce718e4ec618b6
Adam <adamcpenman@gmail.com>
2019-12-18 18:37:12 -0600

    post, put, delete. CRUD working

--- server.js
+++ server.js
@@ -22,5 +22,40 @@ server.get("/:id", async (req, res, next) => {
     }
 })
 
+server.post("/", async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        } 
+        const [id] = await db("accounts").insert(payload)
+        res.json(await db("accounts").where("id", id).first())
+    
+        } catch (err) {
+            next(err)
+        }
+})
+
+server.put("/:id", async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+        await db("accounts").where("id", req.params.id).update(payload)
+        res.json(await db("accounts").where("id", req.params.id).first())
+    } catch (err) {
+        next(err, "Deleted")
+    }
+})
+
+server.delete("/:id", async (req, res, next) => {
+    try {
+        await db("accounts").where("id", req.params.id).del()
+        res.status(204).end()
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = server;
